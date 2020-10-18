b1fa5dc87be3142dc6e37d1ddc0cc5f5a61cf1b3
pdrhenriqueu <43590798+pdrhenriqueu@users.noreply.github.com>
2020-10-18 10:30:59 -0300

    Delete exe003.py

--- exe003.py
+++
@@ -1,9 +0,0 @@
-teste = input("Digite algo: ")
-print("Tipo de sua variavel:", type(teste))
-print("Eh alpha Numerico: ", teste.isalnum())
-print("Eh um letra", teste.isalpha())
-print("Pertence a tabela ascii", teste.isascii())
-print("Esta tudo em minusculo", teste.islower())
-print("Esta tudo em maiusculo", teste.isupper())
-
-
